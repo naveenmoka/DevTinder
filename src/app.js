23987b9559fd2a57b2853b820c18a7571447f1b4
naveenmoka <naveenmoka2004@gmail.com>
2025-12-20 23:44:13 +0530

    Exploring get, post, delete using Postman

--- src/app.js
+++ src/app.js
@@ -2,13 +2,21 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.use("/test", (req, res) => {
-  res.send("Another Route - test");
-});
-app.use((req, res) => {
-  res.send("Server Started");
+app.use("/user", (req, res) => {
+  res.send("Data locally Naveen");
 });
 
+app.get("/user", (req, res) => {
+  res.send({ fistName: "Naveen", lastName: "Moka" });
+});
+
+app.post("/user", (req, res) => {
+  res.send("Data Saved Successfully");
+});
+
+app.delete("/user", (req, res) => {
+  res.send("Data deleted Successfully");
+});
 app.listen(port, () => {
   console.log("Server started running on the port " + port);
 });
